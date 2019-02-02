c44dda417cc598c60465d3bc9ce410e38e9db927
Olivier Cardinaux <ocardinaux@yahoo.fr>
2019-02-02 22:45:12 +0100

    small cosmetic change

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,6 +16,7 @@ RSpec.describe User, type: :model do
       # vérifie si le user est bien valide
       expect(@user).to_be_a User
     end
+    
     describe "#email" do
       # vérifie la présence de l'email
       it { expect(@user).to validate_presence_of(:email) }
@@ -26,12 +27,14 @@ RSpec.describe User, type: :model do
       # vérifie différentes valeurs de l'email qui ne doivent pas être correctes
       pending "vérifie différentes valeurs de l'email qui ne doivent pas être correctes"
     end
+
     describe "#phone_number" do
       # vérifie la présence du phone_number
       it { expect(@user).to validate_presence_of(:phone_number) }
       # verifie différentes valeurs du phone_number qui doivent être correctes
       it { expect(@user.email).to match(/\A(?:(?:\+|00)33[\s.-]{0,3}(?:\(0\)[\s.-]{0,3})?|0)[1-9](?:(?:[\s.-]?\d{2}){4}|\d{2}(?:[\s.-]?\d{3}){2})\z/) }
       # vérifie différentes valeurs du phone_number qui ne doivent pas être correctes
+      # probablement un FactoryBot.build quelquechose
 	  end
   end
 
