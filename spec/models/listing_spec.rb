7f526159faa8e8c66a5cce5c2adb3b832a606657
Olivier Cardinaux <ocardinaux@yahoo.fr>
2019-02-02 21:18:10 +0100

    rspec on listing, user

--- spec/models/listing_spec.rb
+++ spec/models/listing_spec.rb
@@ -3,47 +3,60 @@ require 'rails_helper'
 RSpec.describe Listing, type: :model do
 
   before(:each) do 
-		@listing = FactoryBot.create(:listing)  	
+		@listing = FactoryBot.create(:listing)
   end
 
+      # teste toujours tes factories pour voir si elles sont valides
   it "has a valid factory" do
     # vérifie si la factory est valide
+    expect(build(:listing)).to be_valid
   end
 
   context "validation" do
     it "is valid with valid attributes" do
       # vérifie si le logement est bien valide
+      expect(@listing).to_be_a Listing
     end
+
     describe "#available_beds" do
       # vérifie que available_beds est bien obligatoire
+      it { expect(@listing).to validate_presence_of(:available_beds) }
       # vérifie que available_beds est bien un integer strictement positif
-
+      it {expect(@listing.available_beds).to_be_a(Integer) }
 		end
     describe "#price" do
       # vérifie que le prix est bien obligatoire
+      it { expect(@listing).to validate_presence_of(:price) }
       # vérifie que le prix est bien un integer strictement positif
+      it { expect(@listing.price).to_be_a(Integer) }
 		end
     describe "#description" do
       # vérifie que la description est bien obligatoire
+      it { expect(@listing).to validate_presence_of(:description) }
       # vérifie que la description au bien au moins 140 caractères
+      it { expect(@listing.description.size).to be >= 140 }
 		end
     describe "#welcome_message" do
       # vérifie que le message de bienvenue est bien obligatoire
+      it { expect(@listing).to validate_presence_of(:welcome_message) }
 		end
 	end
-
+  
   context "associations" do
     # vérifie que le logement a plusieurs réservations
+    it { expect(@listing).to have_many(:reservations) }
     # vérifie que le logement appartient à une ville
+    it { expect(@listing.city).to_be_a(City) }
     # vérifie que le logement appartient bien à un admin qui est un User en nom de classe
+    it { expect(@listing.admin).to_be_a(User) }
   end
 
   context "public instance methods" do
     describe "#overlaping_reservation" do
       # vérifie que le listing a bien une méthode d'instance nommée overlaping_reservation? 
+      pending it { expect(@listing.overlaping_reservation?).to exist }
       # vérifie que la méthode marche bien
     end
   end
-
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,28 +8,38 @@ RSpec.describe User, type: :model do
 
   it "has a valid factory" do
     # vérifie si la factory est valide
+    expect(build(:user)).to be_valid
   end
 
   context "validation" do
     it "is valid with valid attributes" do
       # vérifie si le user est bien valide
+      expect(@user).to_be_a User
     end
     describe "#email" do
       # vérifie la présence de l'email
+      it { expect(@user).to validate_presence_of(:email) }
       # vérifie l'unicité de l'email
+      pending "vérifie l'unicité de l'email"
       # verifie différentes valeurs de l'email qui doivent être correctes
-      # vérifie différentes valeurs de l'email qui ne doivent pas être correctes
-		end
-		describe "#phone_number" do
+      it { expect(@user.email).to match(//\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z//) }
+        # vérifie différentes valeurs de l'email qui ne doivent pas être correctes
+        pending "vérifie différentes valeurs de l'email qui ne doivent pas être correctes"
+      end
+    describe "#phone_number" do
       # vérifie la présence du phone_number
+      it { expect(@user).to validate_presence_of(:phone_number) }
       # verifie différentes valeurs du phone_number qui doivent être correctes
+      pending "verifie différentes valeurs du phone_number qui doivent être correctes"
       # vérifie différentes valeurs du phone_number qui ne doivent pas être correctes
 	  end
   end
 
   context "associations" do
     # vérifie que l'utilisateur a plusieurs réservations en tant que guest
+    pending { expect(@user).to have_many(:reservations) }
     # vérifie que l'utilisateur a plusieurs listings en tant qu'admin
+    pending { expect(@user).to have_many(:listings) }
   end
 
 end
